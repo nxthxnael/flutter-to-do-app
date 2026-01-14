90b972fd9da9e0275f8968b42c991fa4508294f8
Nxthxnael <nathanael.mutua.m@gmail.com>
2026-01-14 18:17:14 +0300

    feat: add to-do list containers - add toDoList mock data - add listView builder to list the items in toDoList - add and style the container holding each to do item - modify main.dart to cater for non-final HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final List toDoList = [
+    ['Send Invoice', false],
+    ['Integrate Daraja API', false],
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 206, 206, 206),
+      backgroundColor: const Color(0xFFCECECE),
       appBar: AppBar(
         title: const Text("nxthxnael's to-dos"),
         backgroundColor: Colors.black,
         foregroundColor: Colors.white,
         centerTitle: true,
       ),
-      body: const Center(child: Text('Hello World')),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (BuildContext context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(
+              top: 20,
+              left: 20,
+              right: 20,
+              bottom: 0,
+            ),
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: const Color(0xFF3E3E3E),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Text(
+                toDoList[index][0],
+                style: const TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }

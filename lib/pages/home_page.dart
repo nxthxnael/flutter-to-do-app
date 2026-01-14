d2b438d746d3227875f961008e99bb999c1bd8e6
Nxthxnael <nathanael.mutua.m@gmail.com>
2026-01-14 18:51:53 +0300

    feat: add checkbox - add checkbox to container as child - add handling of state for the checkbox

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/utils/todo_list.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final List toDoList = [
     ['Send Invoice', false],
     ['Integrate Daraja API', false],
@@ -23,7 +28,16 @@ class HomePage extends StatelessWidget {
       body: ListView.builder(
         itemCount: toDoList.length,
         itemBuilder: (BuildContext context, index) {
-          return TodoList(taskName: toDoList[index][0]);
+          return TodoList(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) {
+              // this function is called when the checkbox is tapped
+              setState(() {
+                toDoList[index][1] = !toDoList[index][1];
+              });
+            },
+          );
         },
       ),
     );
--- lib/utils/todo_list.dart
+++ lib/utils/todo_list.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
 class TodoList extends StatelessWidget {
-  const TodoList({super.key, required this.taskName});
+  const TodoList({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    this.onChanged,
+  });
 
   final String taskName;
+  final bool taskCompleted;
+  final Function(bool?)? onChanged;
 
   @override
   Widget build(BuildContext context) {
@@ -15,9 +22,14 @@ class TodoList extends StatelessWidget {
           color: const Color(0xFF3E3E3E),
           borderRadius: BorderRadius.circular(15),
         ),
-        child: Text(
-          taskName,
-          style: const TextStyle(color: Colors.white, fontSize: 18),
+        child: Row(
+          children: [
+            Checkbox(value: taskCompleted, onChanged: onChanged),
+            Text(
+              taskName,
+              style: const TextStyle(color: Colors.white, fontSize: 18),
+            ),
+          ],
         ),
       ),
     );

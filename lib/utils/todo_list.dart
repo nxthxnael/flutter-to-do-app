5b5ff7899255cf098ddf2fd8cf4a140ba496ef6c
user.email <nathanael.mutua.m@gmail.com>
2026-01-15 22:27:48 +0300

    feat: add slidable delete - add flutter_slidable package - add slidable widget to wrap the to do list container - add styling to the slidable action

--- lib/utils/todo_list.dart
+++ lib/utils/todo_list.dart
@@ -1,47 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoList extends StatelessWidget {
   const TodoList({
     super.key,
     required this.taskName,
     required this.taskCompleted,
-    this.onChanged,
+    required this.onChanged,
+    this.deleteFunction,
   });
 
   final String taskName;
   final bool taskCompleted;
   final Function(bool?)? onChanged;
+  final Function(BuildContext)? deleteFunction;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 0),
-      child: Container(
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: const Color(0xFF3E3E3E),
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Row(
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const ScrollMotion(),
           children: [
-            Checkbox(
-              value: taskCompleted,
-              onChanged: onChanged,
-              side: const BorderSide(color: Colors.white),
-            ),
-            Text(
-              taskName,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                decoration: taskCompleted
-                    ? TextDecoration.lineThrough
-                    : TextDecoration.none,
-                decorationColor: Colors.white,
-                decorationThickness: 3,
-              ),
+            SlidableAction(
+              onPressed: deleteFunction,
+              icon: Icons.delete,
+              borderRadius: BorderRadius.circular(15),
+              backgroundColor: Colors.deepPurple,
+              foregroundColor: Colors.white,
+              label: 'Delete',
             ),
           ],
+        ),
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: const Color(0xFF3E3E3E),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                side: const BorderSide(color: Colors.white),
+              ),
+              Text(
+                taskName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                  decorationColor: Colors.white,
+                  decorationThickness: 3,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

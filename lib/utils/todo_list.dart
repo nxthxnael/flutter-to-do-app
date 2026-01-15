f91576a882c48ff9b59430bb51d8b4df5c697d1f
user.email <nathanael.mutua.m@gmail.com>
2026-01-15 20:50:01 +0300

    style: add checkbox styling

--- lib/utils/todo_list.dart
+++ lib/utils/todo_list.dart
@@ -24,10 +24,22 @@ class TodoList extends StatelessWidget {
         ),
         child: Row(
           children: [
-            Checkbox(value: taskCompleted, onChanged: onChanged),
+            Checkbox(
+              value: taskCompleted,
+              onChanged: onChanged,
+              side: const BorderSide(color: Colors.white),
+            ),
             Text(
               taskName,
-              style: const TextStyle(color: Colors.white, fontSize: 18),
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                decoration: taskCompleted
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+                decorationColor: Colors.white,
+                decorationThickness: 3,
+              ),
             ),
           ],
         ),
